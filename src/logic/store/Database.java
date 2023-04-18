f5a6d4e7ed54f6a8bd2239d23d4a69b42ba1f7f5
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-18 18:21:33 +0700

    feat: setup connection to db

--- src/logic/store/Database.java
+++ src/logic/store/Database.java
@@ -2,10 +2,32 @@ package logic.store;
 
 import lombok.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import logic.store.interfaces.*;
 
 @NoArgsConstructor
 @Value
 public class Database implements Parser, Storable {
+    static {
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
+    /**
+     * Setup connection to db
+     * 
+     * @return connection
+     * @throws SQLException
+     */
+    public Connection getConnection() throws SQLException {
+        String url = "jdbc:derby:bnmoDatabase;create=true";
+        Connection conn = DriverManager.getConnection(url);
+        return conn;
+    }
 }
