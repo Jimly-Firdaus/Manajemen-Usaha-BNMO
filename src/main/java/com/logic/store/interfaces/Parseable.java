7ec9a71fead9f6597518552db8522255ca1c85f5
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-20 20:18:36 +0700

    fix: fix interface methods

--- src/main/java/com/logic/store/interfaces/Parseable.java
+++ src/main/java/com/logic/store/interfaces/Parseable.java
@@ -1,10 +1,12 @@
 package com.logic.store.interfaces;
 
+import java.util.List;
+
 public interface Parseable {
     /**
      * Parseable type is Bill, Product, User
     */
-    public void convertFromJSON();
-    public void convertToJSON();
+    public <T> List<T> readFromJSON(Class<T> classType);
+    public <T> void writeData(List<T> data);
 
 }
