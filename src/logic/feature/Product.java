ee2b37df13770b03a6d4bcf9df391042b62dcf67
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-04-23 09:26:23 +0700

    add product method

--- src/logic/feature/Product.java
+++ src/logic/feature/Product.java
@@ -1,15 +1,15 @@
 package logic.feature;
 
 class Product {
-    /*Attribute */
+    /* Attribute */
     private int count;
     private String productName;
     private float basePrice;
     private float boughtPrice;
     private String category;
 
-    /*Constructor */
-    public Product(int count, String productName, float basePrice, float boughtPrice, String category){
+    /* Constructor */
+    public Product(int count, String productName, float basePrice, float boughtPrice, String category) {
         this.count = count;
         this.productName = productName;
         this.basePrice = basePrice;
@@ -17,8 +17,21 @@ class Product {
         this.category = category;
     }
 
-    /*Method */
-    public String getName(){
+    /* Method */
+    public String getName() {
         return this.productName;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public void setCount(int newCount) {
+        this.count = newCount;
+    }
+
+    public boolean isEquals(Product productCompare) {
+        return this.productName.equals(productCompare.productName) && this.basePrice == productCompare.basePrice
+                && this.boughtPrice == productCompare.boughtPrice && this.category.equals(productCompare.category);
     }
 }
