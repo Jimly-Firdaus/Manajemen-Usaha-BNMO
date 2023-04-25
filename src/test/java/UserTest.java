a56a936855555fb80b4e023d13a633c43d5a1dbb
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-25 13:47:09 +0700

    test: fix testing code

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -6,29 +6,7 @@ import com.logic.feature.VIP;
 
 public class UserTest {
     @Test
-    /* Tester */
-    public static void main(String[] args) {
-        // /* Test Customer */
-        // Customer testCst = new Customer(10);
-        // System.out.println(testCst.getId());
-        // System.out.println(testCst.isMadeFirstPurchase());
-
-        // /* Test Member  */
-        // Member testMmb = new Member(100, "xyz", "67890");
-        // System.out.println(testMmb.getId());
-        // System.out.println(testMmb.getName());
-        // System.out.println(testMmb.getPhoneNumber());
-        // System.out.println(testMmb.getPoint());
-
-        // /* Test VIP */
-        // VIP testVIP = new VIP(1, "abc", "12345");
-        // System.out.println(testVIP.getId());
-        // System.out.println(testVIP.getName());
-        // System.out.println(testVIP.getPhoneNumber());
-        // System.out.println(testVIP.getPoint());
-        // System.out.println(testVIP.getDiscountRate());
-
-        /* Test */
+    public void userTest() {
         Customer cst = new Customer(1);
         cst.makePayment();
         Member mmb = cst.upgradeToMember("abc", "123"); /* Upgrade to Member */
