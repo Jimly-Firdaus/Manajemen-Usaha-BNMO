2c1aeeeab644c011937f50fcefb13931209e55d4
billc27 <110593711+billc27@users.noreply.github.com>
2023-04-25 13:43:59 +0700

    refactor: update unit test

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -8,38 +8,34 @@ public class UserTest {
     @Test
     /* Tester */
     public static void main(String[] args) {
-        /* Test Customer */
-        Customer testCst = new Customer(10);
-        System.out.println(testCst.getId());
-        System.out.println(testCst.isMadeFirstPurchase());
+        // /* Test Customer */
+        // Customer testCst = new Customer(10);
+        // System.out.println(testCst.getId());
+        // System.out.println(testCst.isMadeFirstPurchase());
 
-        /* Test Member  */
-        Member testMmb = new Member(100, "xyz", "67890");
-        System.out.println(testMmb.getId());
-        System.out.println(testMmb.getName());
-        System.out.println(testMmb.getPhoneNumber());
-        System.out.println(testMmb.getPoint());
+        // /* Test Member  */
+        // Member testMmb = new Member(100, "xyz", "67890");
+        // System.out.println(testMmb.getId());
+        // System.out.println(testMmb.getName());
+        // System.out.println(testMmb.getPhoneNumber());
+        // System.out.println(testMmb.getPoint());
 
-        /* Test VIP */
-        VIP testVIP = new VIP(1, "abc", "12345");
-        System.out.println(testVIP.getId());
-        System.out.println(testVIP.getName());
-        System.out.println(testVIP.getPhoneNumber());
-        System.out.println(testVIP.getPoint());
-        System.out.println(testVIP.getDiscountRate());
+        // /* Test VIP */
+        // VIP testVIP = new VIP(1, "abc", "12345");
+        // System.out.println(testVIP.getId());
+        // System.out.println(testVIP.getName());
+        // System.out.println(testVIP.getPhoneNumber());
+        // System.out.println(testVIP.getPoint());
+        // System.out.println(testVIP.getDiscountRate());
 
-        /* Test Upgrade */
+        /* Test */
         Customer cst = new Customer(1);
         cst.makePayment();
         Member mmb = cst.upgradeToMember("abc", "123"); /* Upgrade to Member */
 
-        System.out.println(mmb.getId());
-        System.out.println(mmb.getName());
-        System.out.println(mmb.getPhoneNumber());
-        System.out.println(mmb.getPaymentHistory());
         VIP vip = mmb.upgradeToVIP(mmb.getName(), mmb.getPhoneNumber()); /* Upgrade to VIP */
 
-        testVIP = new VIP(1, "abc", "123");
+        VIP testVIP = new VIP(1, "abc", "123");
         assertEquals(vip.getId(), testVIP.getId());
         assertEquals(vip.getName(), testVIP.getName());
         assertEquals(vip.getPhoneNumber(), testVIP.getPhoneNumber());
