148fcfc578f4bbfe8dae8b9617e313e6c2501ad2
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-25 10:28:55 +0700

    refactor: use type annotations

--- src/main/java/com/logic/constant/Payment.java
+++ src/main/java/com/logic/constant/Payment.java
@@ -1,4 +1,13 @@
 package com.logic.constant;
+
+import com.logic.feature.ListOfProduct;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 class Payment {
     /*Attribute */
     private int userID;
