ebc78970cc50836b627fe1dbf22a7241f347fd9e
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-04-30 17:55:21 +0700

    feat: printer

--- src/main/java/com/logic/output/Printer.java
+++ src/main/java/com/logic/output/Printer.java
@@ -1,31 +1,50 @@
 package com.logic.output;
-import lombok.*;
+
 import java.util.*;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.draw.LineSeparator;
+import com.itextpdf.text.Chunk;
+import java.io.FileOutputStream;
 
-import com.logic.feature.Bill;
 import com.logic.constant.Payment;
+import com.logic.feature.Bill;
 
-@NoArgsConstructor
-@Setter
-@Getter
-class Printer{
-    /*Attribute */
-    private ArrayList<Payment> paymentList = new ArrayList<Payment>();
-    private ArrayList<Bill> billList = new ArrayList<Bill>();
-
-    public Payment getCertainPayment(int idx){
-        if(idx < this.paymentList.size()){
-            return this.paymentList.get(idx);
-        }else{
-            return null;
+public class Printer{
+    public static void printPayments(List<Payment> payments) {
+        Document document = new Document();
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream("../Payments.pdf"));
+            document.open();
+            LineSeparator ls = new LineSeparator();
+            for (Payment payment : payments) {
+                document.add(new Paragraph(payment.toString()));
+                document.add(new Chunk(ls));
+                // Sleep for 1 second
+                Thread.sleep(100);
+            }
+            document.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public Bill getCertainBill(int idx){
-        if(idx < this.billList.size()){
-            return this.billList.get(idx);
-        }else{
-            return null;
+    public static void printBills(List<Bill> bills) {
+        Document document = new Document();
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream("../Bills.pdf"));
+            document.open();
+            LineSeparator ls = new LineSeparator();
+            for (Bill bill : bills) {
+                document.add(new Paragraph(bill.toString()));
+                document.add(new Chunk(ls));
+                // Sleep for 1 second
+                Thread.sleep(100);
+            }
+            document.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
