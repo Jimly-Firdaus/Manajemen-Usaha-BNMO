d8540c35df73875684779d80bdd2bbe6ce3020a2
billc27 <110593711+billc27@users.noreply.github.com>
2023-05-02 10:20:34 +0700

    refactor: change style and add back button

--- src/main/java/com/gui/pages/RegistrationPage.java
+++ src/main/java/com/gui/pages/RegistrationPage.java
@@ -9,6 +9,8 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import com.gui.interfaces.PageSwitcher;
+import javafx.geometry.Insets;
+import com.gui.components.*;
 
 public class RegistrationPage extends VBox {
 
@@ -17,11 +19,6 @@ public class RegistrationPage extends VBox {
         Font titleFont = Font.font("Georgia", FontWeight.BOLD,18);
         Font textFont = Font.font("Times New Roman", 15);
 
-        // Create a grid pane
-        GridPane gridPane = new GridPane();
-        gridPane.setVgap(5);
-        gridPane.setHgap(5);
-
         // Title Label
         Label titleLabel = new Label("MEMBER REGISTRATION");
         titleLabel.setFont(titleFont);
@@ -29,6 +26,11 @@ public class RegistrationPage extends VBox {
         // HBox for Title Label
         HBox titleHLayout = new HBox(titleLabel);
         titleHLayout.setAlignment(Pos.CENTER);
+
+        // Create a grid pane
+        GridPane gridPane = new GridPane();
+        gridPane.setVgap(5);
+        gridPane.setHgap(5);
 
         // Create labels and text fields for name and phone number input
         Label nameLabel = new Label("Name:");
@@ -45,25 +47,34 @@ public class RegistrationPage extends VBox {
         TextField phoneField = new TextField();
         gridPane.add(phoneField, 1, 1);
 
-        // Create a button for submitting the registration form
-        Button submitButton = new Button("Register");
-        gridPane.add(submitButton, 1, 2);
-        submitButton.setStyle(
-                "-fx-background-color: blue;" +
-                "-fx-text-fill: #ffffff;" +
-                "-fx-font-family: 'Georgia';" +
-                "-fx-font-size: 12px;"
-        );
-
         // Create a HBox container and add the GridPane to it
         HBox inputHLayout = new HBox(gridPane);
         inputHLayout.setAlignment(Pos.CENTER);
 
+        // Submit Button
+        Button submitButton = new Button("Register");
+        HBox submitHLayout = new HBox(submitButton);
+        submitHLayout.setAlignment(Pos.CENTER);
+        submitButton.setStyle(
+                "-fx-background-color: blue;" +
+                "-fx-text-fill: #ffffff;" +
+                "-fx-font-family: 'Georgia';" +
+                "-fx-font-size: 12px;" +
+                "-fx-pref-width: 250px"
+        );
+
+        // Back Button
+        BaseButton backButton = new BaseButton("Back");
+        HBox backHLayout = new HBox(backButton);
+        backHLayout.setAlignment(Pos.BOTTOM_RIGHT);
+
         // Create a VBox as a container
         VBox container = new VBox();
-        container.getChildren().addAll(titleHLayout, inputHLayout);
-        container.setSpacing(30);
-        container.setAlignment(Pos.CENTER);
+        container.getChildren().addAll(titleHLayout, inputHLayout, submitHLayout, backHLayout);
+
+        VBox.setMargin(titleHLayout, new Insets(200, 0, 35, 0));
+        VBox.setMargin(submitHLayout, new Insets(15, 0, 75, 0));
+        VBox.setMargin(backHLayout, new Insets(95, 30, 0, 0));
 
         // Submit Button
         submitButton.setOnAction(event -> {
