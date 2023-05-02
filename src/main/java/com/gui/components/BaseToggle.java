498b123ee4c63e5d96ad9161f6133d46b623cc32
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-05-02 21:56:49 +0700

    fix(components): base toggle now can change to vertical/horizontal

--- src/main/java/com/gui/components/BaseToggle.java
+++ src/main/java/com/gui/components/BaseToggle.java
@@ -1,15 +1,17 @@
 package com.gui.components;
 
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.RadioButton;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 
 public class BaseToggle extends HBox {
     private ToggleGroup group;
 
-    public BaseToggle(String opt1, String opt2, Button button) {
+    public BaseToggle(String opt1, String opt2, Button button, boolean isVertical) {
         this.group = new ToggleGroup();
         RadioButton radio1 = new RadioButton(opt1);
         radio1.setToggleGroup(group);
@@ -18,8 +20,21 @@ public class BaseToggle extends HBox {
 
         // TODO: styles
 
-        getChildren().addAll(radio1, radio2);
-
+        // getChildren().addAll(radio1, radio2);
+        
+        if (isVertical) {
+            VBox vbox = new VBox(radio1, radio2);
+            vbox.setAlignment(Pos.CENTER);
+            vbox.setSpacing(10);
+            getChildren().add(vbox);
+        } else {
+            HBox hbox = new HBox(radio1, radio2);
+            hbox.setAlignment(Pos.CENTER);
+            hbox.setSpacing(10);
+            getChildren().add(hbox);
+        }
+        setAlignment(Pos.CENTER);
+        
         group.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue == null) {
                 button.setDisable(true);
