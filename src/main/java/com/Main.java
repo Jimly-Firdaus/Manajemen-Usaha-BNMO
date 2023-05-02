a89dd1f763ecca31bd8ed71df476a2d603e36eda
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-05-02 23:56:45 +0700

    add menu and tab

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,21 +1,33 @@
 package com;
 
 import javafx.application.Application;
-// import javafx.scene.Scene;
-// import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.geometry.Insets;
 import javafx.geometry.Rectangle2D;
-import javafx.stage.Screen;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
 import com.gui.Router;
-import javafx.scene.layout.StackPane;
+import com.gui.pages.*;
 
 public class Main extends Application {
-    // private StackPane root = new StackPane();
+    private TabPane tabPane;
+    private Stage primaryStage;
+    Router router;
 
     @Override
     public void start(Stage primaryStage) {
-        StackPane root = new StackPane();
+        this.primaryStage = primaryStage;
+        router = new Router(primaryStage);
+
+        BorderPane root = new BorderPane();
         Scene scene = new Scene(root, 1080, 608);
 
         // Center the window
@@ -25,8 +37,52 @@ public class Main extends Application {
         primaryStage.setX(centerX);
         primaryStage.setY(centerY);
 
-        Router router = new Router(primaryStage);
-        router.gotoMainPage();
+        // Make menubar
+        MenuBar menuBar = new MenuBar();
+        root.setTop(menuBar);
+
+        // Make Menu dropdown
+        Menu halamanMenu = new Menu("Menu");
+
+        // Set Item in Menu
+        MenuItem halaman1 = new MenuItem("Dashboard");
+        halaman1.setOnAction(event -> createNewTab("Dashboard"));
+        MenuItem halaman2 = new MenuItem("Membership");
+        halaman2.setOnAction(event -> createNewTab("Membership"));
+        MenuItem halaman3 = new MenuItem("Halaman 3");
+        halaman3.setOnAction(event -> createNewTab("Halaman 3"));
+        halamanMenu.getItems().addAll(halaman1, halaman2, halaman3);
+        menuBar.getMenus().add(halamanMenu);
+        menuBar.setPadding(new Insets(10));
+
+        // Make tabPane
+        tabPane = new TabPane();
+        root.setCenter(tabPane);
+
+        // Default tab "Dashboard"
+        createNewTab("Dashboard");
+
+        // router.gotoMainPage();
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    // Create a new tab according to the menu options
+    private void createNewTab(String menuName) {
+        Tab tab = new Tab(menuName);
+        tab.setClosable(true);
+
+        if (menuName.equals("Dashboard")) {
+            tab.setContent(new MainPage(router));
+        } else if (menuName.equals("Membership")) {
+            tab.setContent(new MembershipDeactivationPage(router));
+        } else {
+            tab.setContent(new Label("Lanjutkan"));
+        }
+
+        // Add tab to tabpane
+        tabPane.getTabs().add(tab);
+        tabPane.getSelectionModel().select(tab);
     }
 
     public static void main(String[] args) {
