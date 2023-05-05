fb0655021ed904bd1f99fc154501ff9ab72abeba
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-05-05 13:35:05 +0700

    fix: router return Node

--- src/main/java/com/gui/Router.java
+++ src/main/java/com/gui/Router.java
@@ -1,7 +1,10 @@
 package com.gui;
 
 import javafx.scene.Scene;
+import javafx.scene.Node;
 import javafx.stage.Stage;
+import lombok.Value;
+
 import java.util.List;
 
 import com.gui.interfaces.PageSwitcher;
@@ -10,55 +13,51 @@ import com.gui.pages.*;
 import com.logic.feature.interfaces.IBill;
 import com.logic.constant.interfaces.IPayment;
 
+@Value
 public class Router implements PageSwitcher {
     private Stage stage;
+    private String inputPath = "";
+    
+
+
+
+
+    // TODO : Find a way to share resources to all pages by references
 
     public Router(Stage stage) {
         this.stage = stage;
     }
 
-    public void gotoMainPage() {
+    public Node gotoMainPage() {
         MainPage mainPage = new MainPage(this);
         mainPage.setPrefSize(1080, 608);
-        Scene scene = new Scene(mainPage);
-        stage.setTitle("Home");
-        stage.setScene(scene);
-        stage.show();
+        return mainPage;
     }
 
-    public void gotoPageLaporan(List<IBill> lsBills, List<IPayment> lsPayments) {
+    // If resources is visible to all, then the args is not needed here
+    public Node gotoPageLaporan(List<IBill> lsBills, List<IPayment> lsPayments) {
         PageLaporan pageLaporan = new PageLaporan(this, lsBills, lsPayments, this.stage);
         pageLaporan.setPrefSize(1080, 608);
-        Scene scene = new Scene(pageLaporan);
-        stage.setTitle("Laporan");
-        stage.setScene(scene);
-        stage.show();
+        return pageLaporan;
     }
 
     // Add your pages below here
-    public void gotoRegistrationPage() {
+    public Node gotoRegistrationPage() {
         RegistrationPage pageRegister = new RegistrationPage(this);
         pageRegister.setPrefSize(1080, 608);
-        Scene scene = new Scene(pageRegister);
-        stage.setTitle("Register");
-        stage.setScene(scene);
-        stage.show();
+        return pageRegister;
     }
 
-    public void gotoUpdateInfoPage() {
+    public Node gotoUpdateInfoPage() {
         UpdateInfoPage pageUpdateInfo = new UpdateInfoPage(this);
         pageUpdateInfo.setPrefSize(1080, 608);
-        Scene scene = new Scene(pageUpdateInfo);
-        stage.setScene(scene);
-        stage.show();
+        return pageUpdateInfo;
     }
 
-    public void gotoMembershipDeactivationPage() {
+    public Node gotoMembershipDeactivationPage() {
         MembershipDeactivationPage pageMembershipDeactivation = new MembershipDeactivationPage(this);
         pageMembershipDeactivation.setPrefSize(1080, 608);
-        Scene scene = new Scene(pageMembershipDeactivation);
-        stage.setScene(scene);
-        stage.show();
+        return pageMembershipDeactivation;
     }
     
 }
