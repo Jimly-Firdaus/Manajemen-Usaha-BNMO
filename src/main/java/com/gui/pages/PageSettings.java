ac017307762ed7e71b28f2c071839156ecff837a
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-05-05 23:07:20 +0700

    chore: update settings page

--- src/main/java/com/gui/pages/PageSettings.java
+++ src/main/java/com/gui/pages/PageSettings.java
@@ -1,7 +1,5 @@
 package com.gui.pages;
 
-import com.gui.interfaces.PageSwitcher;
-
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
@@ -9,27 +7,30 @@ import javafx.scene.layout.HBox;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.geometry.Insets;
-
 import java.io.File;
+import java.util.List;
 
+import com.gui.interfaces.PageSwitcher;
 import com.gui.components.*;
+import com.logic.store.interfaces.Parseable;
 import com.logic.store.ParserJSON;
 import com.logic.store.ParserXML;
+import com.logic.feature.Bill;
 
 public class PageSettings extends VBox {
     private Stage stage;
     private String pathLabel = "";
-    private Boolean databaseReloadStatus = true;
     private String databaseReloadText = "";
 
-    // Need reference resource to fill in from Parser
-    public PageSettings(PageSwitcher pageCaller, String inputPath, Stage stage) {
+    // Need reference resource to fill in from Parser (Bills, and other)
+    // TODO: add more objects that are needs to be retrieved from data store
+    public PageSettings(PageSwitcher pageCaller, String inputPath, List<Bill> storedBills, Stage stage) {
         this.stage = stage;
 
         // input file path dialog
         BaseCard fileDialogCard = new BaseCard("Input File Option", "");
         HBox fileDialogBox = new HBox();
-        fileDialogBox.setSpacing(10); 
+        fileDialogBox.setSpacing(10);
         Label inputPathLabel = new Label("Data Store Path: " + pathLabel);
         BaseButton openFile = new BaseButton("Open File");
         fileDialogBox.getChildren().addAll(openFile, inputPathLabel);
@@ -37,18 +38,17 @@ public class PageSettings extends VBox {
         HBox.setMargin(fileDialogBox, new Insets(0, 0, 0, 20));
         fileDialogCard.getChildren().addAll(fileDialogBox);
         fileDialogCard.setAlignment(Pos.CENTER_LEFT);
-        
+
         BaseButton reloadDatabase = new BaseButton("Refresh Database");
-        Label reloadDatabaseStatus = new Label(""); 
-        reloadDatabase.setOnAction(event -> onReloadDatabase(reloadDatabaseStatus));
+        Label reloadDatabaseStatus = new Label("");
+        reloadDatabase.setOnAction(event -> onReloadDatabase(reloadDatabaseStatus, storedBills, Bill.class));
         openFile.setOnAction(event -> this.chooseOutputPath(inputPath, inputPathLabel, reloadDatabaseStatus));
         // TODO : 2
         // Delete or unplug plugin
-        
-        
+
         // TODO : 3
         // Other config should be done in here
-        
+
         this.getChildren().addAll(fileDialogCard);
     }
 
@@ -58,8 +58,7 @@ public class PageSettings extends VBox {
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("JSON Files", "*.json"),
                 new FileChooser.ExtensionFilter("XML Files", "*.xml"),
-                new FileChooser.ExtensionFilter("OBJ Files", "*.obj")
-        );
+                new FileChooser.ExtensionFilter("OBJ Files", "*.obj"));
         File selectedFile = fileChooser.showOpenDialog(stage);
         if (selectedFile != null) {
             String inputPath = selectedFile.getAbsolutePath();
@@ -68,15 +67,31 @@ public class PageSettings extends VBox {
             label.setText("Data Store Path: " + this.pathLabel);
             System.out.println("User file: " + inputPath);
         }
-        this.databaseReloadStatus = true;
         this.databaseReloadText = "Reload required!";
         databaseStatusText.setText(this.databaseReloadText);
     }
 
-    public void onReloadDatabase(Label label) {
+    public <T> void onReloadDatabase(Label label, List<T> storedData, Class<T> classType) {
+        String extension = this.pathLabel.substring(this.pathLabel.lastIndexOf(".") + 1);
 
+        List<T> newData = null;
 
-        this.databaseReloadStatus = false;
+        switch (extension) {
+            case "json":
+                Parseable jsonParser = new ParserJSON(this.pathLabel);
+                newData = jsonParser.readData(classType);
+                break;
+            case "xml":
+                Parseable xmlParser = new ParserXML(this.pathLabel);
+                newData = xmlParser.readData(classType);
+                break;
+        }
+
+        if (newData != null) {
+            storedData.clear();
+            storedData.addAll(newData);
+        }
+
         this.databaseReloadText = "";
         label.setText(this.databaseReloadText);
     }
