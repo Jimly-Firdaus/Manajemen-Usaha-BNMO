0f57be6248bf90a75328d38b46b7dcf452b86ba6
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-05-06 15:17:02 +0700

    refactor: change map to list

--- src/main/java/com/logic/feature/ListOfProduct.java
+++ src/main/java/com/logic/feature/ListOfProduct.java
@@ -7,7 +7,7 @@ import lombok.*;
 @Getter
 public class ListOfProduct implements Serializable{
     /*Attribute */
-    private Map<String, Product> productList = new HashMap<String, Product>();
+    private List<Product> productList = new ArrayList<Product>(); 
 
     /*Method */
     public int getProductCount(){
@@ -15,28 +15,33 @@ public class ListOfProduct implements Serializable{
     }
 
     public Product searchProduct(String productName){
-        if(this.productList.get(productName) != null){
-            return this.productList.get(productName);
+        for (Product product : this.productList){
+            if (product.getProductName().equals(productName)) {
+                return product;
+            }
         }
-        else{
-            return null;
-        }
+        return null;
     }
 
     public void addProduct(Product product){
-        this.productList.put(product.getProductName(), product);
+        this.productList.add(product);
     }
 
     public void removeProduct(String productName){
-        this.productList.remove(productName);
+        for (int i = 0; i < productList.size(); i++) {
+            if (productList.get(i).getProductName().equals(productName)) {
+                productList.remove(i);
+                break;
+            }
+        }
     }
 
     public void printAllProduct(){
         if(getProductCount() == 0){
             System.out.println("No product");
         }else{
-            for (String key : this.productList.keySet()) {
-                System.out.println(key);
+            for (Product product : productList) {
+                System.out.println(product.getProductName());
             }
         }
     }
@@ -45,21 +50,10 @@ public class ListOfProduct implements Serializable{
     public String toString() {
         StringBuilder sb = new StringBuilder();
         int i = 1;
-        for (Product product : productList.values()) {
+        for (Product product : productList) {
             sb.append(i).append(". ").append(product.toString()).append(",\n");
             i++;
         }
         return sb.toString();
-    }
-
-    public static void main(String[] args) {
-        Product product1 = new Product(2, "product-1", 100, 100, "desc-1");
-        Product product2 = new Product(3, "product-2", 100, 100, "desc-2");
-        Product product4 = new Product(4, "product-3", 100, 100, "desc-3");
-        ListOfProduct productList = new ListOfProduct();
-        productList.printAllProduct();
-        productList.addProduct(product1);
-        productList.printAllProduct();
-
     }
 }
