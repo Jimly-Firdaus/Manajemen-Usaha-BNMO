42fcfbc02a3f6e78494bfb9e7c293344493a7267
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-05-06 13:25:03 +0700

    refactor: shoppingpage func to inventorypage

--- src/main/java/com/gui/interfaces/PageSwitcher.java
+++ src/main/java/com/gui/interfaces/PageSwitcher.java
@@ -10,5 +10,5 @@ public interface PageSwitcher {
     Node gotoMembershipDeactivationPage();
     Node gotoUserPage();
     Node gotoSettingsPage();
-    Node gotoShoppingPage();
+    Node gotoInventoryPage();
 }
