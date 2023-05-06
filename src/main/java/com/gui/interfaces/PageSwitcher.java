3e43d8a2bb241a05b5296a0f937614b95a809c87
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-05-06 08:43:17 +0700

    chore: update interface

--- src/main/java/com/gui/interfaces/PageSwitcher.java
+++ src/main/java/com/gui/interfaces/PageSwitcher.java
@@ -1,14 +1,10 @@
 package com.gui.interfaces;
 
-import java.util.List;
 import javafx.scene.Node;
-
-import com.logic.feature.interfaces.IBill;
-import com.logic.constant.interfaces.IPayment;
 
 public interface PageSwitcher {
     Node gotoMainPage();
-    Node gotoPageLaporan(List<IBill> lsBills, List<IPayment> lsPayments);
+    Node gotoPageLaporan();
     Node gotoRegistrationPage();
     Node gotoUpdateInfoPage();
     Node gotoMembershipDeactivationPage();
