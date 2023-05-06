dc660de939e4178565f669d742380394a826e442
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-05-06 11:48:34 +0700

    chore(store/interface): update interface for single object

--- src/main/java/com/logic/store/interfaces/Storeable.java
+++ src/main/java/com/logic/store/interfaces/Storeable.java
@@ -3,6 +3,8 @@ package com.logic.store.interfaces;
 import java.util.List;
 
 public interface Storeable {
-    public <T> void storeData(List<T> data);
-    public <T> List<T> getData(Class<T> classType);
+    public <T> void storeDatas(List<T> data);
+    public <T> List<T> getDatas(Class<T> classType);
+    public <T> void storeData(T data);
+    public <T> T getData(Class<T> classType);
 }
