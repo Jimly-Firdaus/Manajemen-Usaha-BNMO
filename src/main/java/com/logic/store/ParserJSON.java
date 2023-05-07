366da80537f74efc0f9c483eb136175db2d26f7e
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-05-07 11:58:13 +0700

    fix: fix to read superclass

--- src/main/java/com/logic/store/ParserJSON.java
+++ src/main/java/com/logic/store/ParserJSON.java
@@ -68,69 +68,73 @@ public class ParserJSON implements Parseable {
         Constructor<T> constructor = classType.getDeclaredConstructor();
         T item = constructor.newInstance();
 
-        for (Field field : classType.getDeclaredFields()) {
-            field.setAccessible(true);
-            String fieldName = field.getName();
-            if (json.has(fieldName)) {
-                Object val = json.get(fieldName);
-                if (field.getType().isAssignableFrom(ListOfProduct.class)) {
-                    JSONObject jsonObject = (JSONObject) val;
-                    ListOfProduct listOfProduct = new ListOfProduct();
-                    for (Field elementField : ListOfProduct.class.getDeclaredFields()) {
-                        elementField.setAccessible(true);
-                        Object elementVal = jsonObject.get(elementField.getName());
-                        if (elementField.getType().isAssignableFrom(List.class)) {
-                            Type genericType = elementField.getGenericType();
-                            if (genericType instanceof ParameterizedType) {
-                                ParameterizedType parameterizedType = (ParameterizedType) genericType;
-                                Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
-                                if (actualTypeArguments.length == 1
-                                        && actualTypeArguments[0] instanceof Class) {
-                                    Class<?> listElementType = (Class<?>) actualTypeArguments[0];
-                                    JSONArray jsonArrays = (JSONArray) elementVal;
-                                    List<Object> list = new ArrayList<>();
-                                    for (int j = 0; j < jsonArrays.length(); j++) {
-                                        JSONObject listElementJson = jsonArrays.getJSONObject(j);
-                                        Object element = listElementType.getDeclaredConstructor().newInstance();
-                                        for (Field listElementField : listElementType.getDeclaredFields()) {
-                                            listElementField.setAccessible(true);
-                                            Object listElementVal = listElementJson
-                                                    .get(listElementField.getName());
-                                            listElementField.set(element, listElementVal);
+        Class<?> currentClass = classType;
+        while (currentClass != null) {
+
+            for (Field field : currentClass.getDeclaredFields()) {
+                field.setAccessible(true);
+                String fieldName = field.getName();
+                if (json.has(fieldName)) {
+                    Object val = json.get(fieldName);
+                    if (field.getType().isAssignableFrom(ListOfProduct.class)) {
+                        JSONObject jsonObject = (JSONObject) val;
+                        ListOfProduct listOfProduct = new ListOfProduct();
+                        for (Field elementField : ListOfProduct.class.getDeclaredFields()) {
+                            elementField.setAccessible(true);
+                            Object elementVal = jsonObject.get(elementField.getName());
+                            if (elementField.getType().isAssignableFrom(List.class)) {
+                                Type genericType = elementField.getGenericType();
+                                if (genericType instanceof ParameterizedType) {
+                                    ParameterizedType parameterizedType = (ParameterizedType) genericType;
+                                    Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
+                                    if (actualTypeArguments.length == 1
+                                            && actualTypeArguments[0] instanceof Class) {
+                                        Class<?> listElementType = (Class<?>) actualTypeArguments[0];
+                                        JSONArray jsonArrays = (JSONArray) elementVal;
+                                        List<Object> list = new ArrayList<>();
+                                        for (int j = 0; j < jsonArrays.length(); j++) {
+                                            JSONObject listElementJson = jsonArrays.getJSONObject(j);
+                                            Object element = listElementType.getDeclaredConstructor().newInstance();
+                                            for (Field listElementField : listElementType.getDeclaredFields()) {
+                                                listElementField.setAccessible(true);
+                                                Object listElementVal = listElementJson
+                                                        .get(listElementField.getName());
+                                                listElementField.set(element, listElementVal);
+                                            }
+                                            list.add(element);
                                         }
-                                        list.add(element);
+                                        elementVal = list;
                                     }
-                                    elementVal = list;
                                 }
                             }
+                            elementField.set(listOfProduct, elementVal);
                         }
-                        elementField.set(listOfProduct, elementVal);
-                    }
-                    val = listOfProduct;
-                } else if (field.getType().isAssignableFrom(ArrayList.class)) {
-                    // Get the generic type of the ArrayList
-                    ParameterizedType arrayListType = (ParameterizedType) field.getGenericType();
-                    Class<?> arrayListElementType = (Class<?>) arrayListType.getActualTypeArguments()[0];
+                        val = listOfProduct;
+                    } else if (field.getType().isAssignableFrom(ArrayList.class)) {
+                        // Get the generic type of the ArrayList
+                        ParameterizedType arrayListType = (ParameterizedType) field.getGenericType();
+                        Class<?> arrayListElementType = (Class<?>) arrayListType.getActualTypeArguments()[0];
 
-                    // Parse the JSON array into an ArrayList
-                    JSONArray jsonArray = (JSONArray) val;
-                    ArrayList<Object> arrayList = new ArrayList<>();
-                    for (int j = 0; j < jsonArray.length(); j++) {
-                        JSONObject jsonObject = jsonArray.getJSONObject(j);
-                        Object element = readData(arrayListElementType, jsonObject);
-                        arrayList.add(element);
+                        // Parse the JSON array into an ArrayList
+                        JSONArray jsonArray = (JSONArray) val;
+                        ArrayList<Object> arrayList = new ArrayList<>();
+                        for (int j = 0; j < jsonArray.length(); j++) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(j);
+                            Object element = readData(arrayListElementType, jsonObject);
+                            arrayList.add(element);
+                        }
+                        val = arrayList;
+                    } else if (!field.getType().isPrimitive() && !field.getType().isAssignableFrom(String.class)) {
+                        // Handle fields of custom class types
+                        JSONObject jsonObject = (JSONObject) val;
+                        Object object = readData(field.getType(), jsonObject);
+                        val = object;
                     }
-                    val = arrayList;
-                } else if (!field.getType().isPrimitive() && !field.getType().isAssignableFrom(String.class)) {
-                    // Handle fields of custom class types
-                    JSONObject jsonObject = (JSONObject) val;
-                    Object object = readData(field.getType(), jsonObject);
-                    val = object;
+                    field.set(item, val);
                 }
-                field.set(item, val);
             }
+            currentClass = currentClass.getSuperclass();
         }
-
         return item;
 
     }
